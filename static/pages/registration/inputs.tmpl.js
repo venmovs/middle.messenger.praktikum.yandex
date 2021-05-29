7bdb876a37d72c6bb101628a130b8ef729e72248
vigen <vigen94@icloud.com>
2021-05-29 10:20:14 +0300

    start makes tmpls

---
+++ static/pages/registration/inputs.tmpl.js
@@ -0,0 +1,8 @@
+export default `
+{{#each data}}
+<label class="flex direction-column align-item-center mb-1">
+    <span class="text-primary text-primary_white">{{this.tittle}}</span>
+<input class="custom-text-input text-primary" name="{{this.name}}" type="{{this.type}}">
+    </label>
+{{/each}}
+`.trim();
--- static/pages/registration/registration.js
+++ static/pages/registration/registration.js
@@ -1,6 +1,28 @@
 'use strict';
+import Handlebars from 'handlebars';
+import inputsTmpl from './inputs.tmpl';
 
-window.addEventListener('load', function () {
+window.addEventListener('DOMContentLoaded', function () {
+
+    function inputTemplate(){
+
+        let data = [
+            {tittle: 'Имя', name: 'Name', type: 'text'},
+            {tittle: 'Телефон', name: 'Phone', type: 'tel'},
+            {tittle: 'Почта', name: 'Mail', type: 'email'},
+            {tittle: 'Пароль', name: 'Password', type: 'password'},
+            {tittle: 'Пароль(повторите еще раз)', name: 'PasswordConfirm', type: 'password'}
+        ];
+
+        let template = Handlebars.compile(inputsTmpl)({
+            data: data
+        });
+
+        document.getElementById('output').innerHTML = template;
+    }
+
+    inputTemplate();
+
 
     let registrationForm = document.getElementById('registrationForm');
     let registrationButton = document.getElementById('registrationButton');
