380a1da2e04052e2888e4f94690a95cd1fdca615
vigen <vigen94@icloud.com>
2021-10-18 21:29:35 +0300

    add all dependency

---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HandlebarsPlugin = require('handlebars-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './static/pages/app.ts'),
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json'],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000,
+    },
+    module: {
+        rules: [
+            { test: /\.svg$/, use: 'svg-inline-loader' },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images',
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(woff|woff2|ttf|otf|eot)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'fonts',
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass'),
+                        },
+                    },
+                    { loader: MiniCssExtractPlugin.loader },
+                ],
+            },
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            configFile: path.resolve(__dirname, 'tsconfig.json'),
+                        },
+                    },
+                ],
+                exclude: /(node_modules)/,
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'foo.bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './static/pages/index.html',
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css',
+        }),
+
+    ],
+    mode: 'production',
+};
